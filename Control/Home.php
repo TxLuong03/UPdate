69aa943f78a0265a462d92141a0dc9f72b1cfffd
Lương <133470672+TxLuong03@users.noreply.github.com>
2024-07-05 11:27:32 +0700

    Update Home.php

--- Control/Home.php
+++ Control/Home.php
@@ -1,1 +1,57 @@
+<?php 
+  namespace AD\control;
+  use AD\model\home;
 
+class HomeControl{
+    public function home(){
+        require_once "./view/homepage.php";
+    }
+
+    public function admin(){
+        require_once "./View_Ad/Ad.php";
+    }
+
+    public function BannerTop(){
+        require_once "./View_Ad/Banner_Top.php";
+    }
+
+    public function BannerMid(){
+        require_once "./View_Ad/Banner_Mid.php";
+    }
+
+    public function BannerBot(){
+        require_once "./View_Ad/Banner_Bot.php";
+    }
+    public function BannerDream(){
+        require_once "./View_Ad/Banner_Dream.php.php";
+    }
+    
+
+    public function Banner_Slide_Mid_1(){
+        require_once "./View_Ad/Banner_Slide_Mid_1.php";
+    }
+
+    public function Banner_Slide_Mid_2(){
+        require_once "./View_Ad/Banner_Slide_Mid_2.php";
+    }
+    public function Banner_Slide_Mid_3(){
+        require_once "./View_Ad/Banner_Slide_Mid_3.php";
+    }
+    public function processingBannerDream(){
+        require_once "./View_Ad/Banner_Dream_Update.php";
+    }
+    public function processingBannerSlideMid1Update(){
+        require_once "./View_Ad/Banner_Slide_Mid_2_Update.php";
+    }
+    public function processingBannerSlideMid2Update(){
+        require_once "./View_Ad/Banner_Slide_Mid_2_Update.php";
+    }
+    public function processingBannerSlideMid3Update(){
+        require_once "./View_Ad/Banner_Slide_Mid_3_Update.php";
+    }
+
+    
+
+}
+
+?>
