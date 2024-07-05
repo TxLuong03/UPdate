6a34764e9ca883149f14ad3a7e67f4d396b523e3
Lương <133470672+TxLuong03@users.noreply.github.com>
2024-07-05 10:24:31 +0700

    Update connect.php

--- connect/connect.php
+++ connect/connect.php
@@ -1,14 +1,15 @@
-?<php
+<?php
         $server = "localhost";
         $username = "root";
         $password = "";
         $dbName = "myDataWeb";
 
-        $this->conn = new mysqli($server, $username, $password, $dbName);
-         
+        $conn = new mysqli($server, $username, $password, $dbName);
+
         if ($conn->connect_error) {
             die("Connection failed: ". $conn->connect_error);
         }else{
             echo "Connected successfully";
         }
+
 ?>
