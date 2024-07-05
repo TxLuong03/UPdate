6ede6df7a12587a3c8ab513ab306bfd825b8c0f2
Lương <133470672+TxLuong03@users.noreply.github.com>
2024-07-05 10:12:31 +0700

    Update connect.php

--- connect/connect.php
+++ connect/connect.php
@@ -1,1 +1,8 @@
+?<php
+        $server = "localhost";
+        $username = "root";
+        $password = "";
+        $dbName = "myDataWeb";
 
+        $this->conn = new mysqli($server, $username, $password, $dbName);
+?>
