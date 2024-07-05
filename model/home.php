d58abdc09955b566e670cac67c0302eeda300a9a
Lương <133470672+TxLuong03@users.noreply.github.com>
2024-07-05 11:15:46 +0700

    Update home.php

--- model/home.php
+++ model/home.php
@@ -1,1 +1,21 @@
+<?php
+namespace AD\model;
 
+use mysqli;
+
+class home{
+    private $conn;
+    public function __construct(){
+        $server = "localhost";
+        $username = "root";
+        $password = "";
+        $dbName = "myDataWeb";
+
+        $this->conn = new mysqli($server, $username, $password, $dbName);
+    }
+
+}
+
+
+
+?>
