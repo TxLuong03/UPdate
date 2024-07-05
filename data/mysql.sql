0bbda3a79b3990e8466634c0bf53ab7cd38b387c
Lương <133470672+TxLuong03@users.noreply.github.com>
2024-07-05 10:39:40 +0700

    Update mysql.sql

--- data/mysql.sql
+++ data/mysql.sql
@@ -1,1 +1,13 @@
+SET SQL_MODE= "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time="+7";
 
+CREATE TABLE `account` (
+  `id` int(5)  NOT NULL;
+  `name` nvarchar(50)  DEFAULT NULL;
+  `pass` nvarchar(20)  DEFAULT NULL;
+  `email` nvarchar(100) DEFAULT NULL;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `account` (`id`, `username`, `password`, `email`,) VALUES
+(1, 'Luong', '123', 'a@gmail.com', NULL, NULL);
