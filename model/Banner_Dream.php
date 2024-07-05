378cc28179e5e1a522452c1b171969a3cfd45ee8
Lương <133470672+TxLuong03@users.noreply.github.com>
2024-07-05 15:54:25 +0700

    Update Banner_Dream.php

--- model/Banner_Dream.php
+++ model/Banner_Dream.php
@@ -1,1 +1,50 @@
+<?php
+  namespace AD\model;
+  use mysqli;
+  use Exception;
 
+  class bannerdream
+    private $conn;
+    public function con()
+    {
+      $servername="localhost";
+      $username="root";
+      $password="";
+      $dbname="mysql";
+
+      $this=>$conn=new mysqli($servername,$username,$password,$dbname);
+    }
+
+    public function getA()
+
+      {
+          $list=[];
+        $sql=" SELECT * FROM bannerdream";
+        $rs=$this->$conn->query($sql);
+        while($row=$rs-> fetch_ossoc())
+          {
+            $list[] = $row;
+          }
+        return $list;
+        
+      }
+      public function updateBannerDream($is,$title,$img,$des)
+      {
+        $sql="UPDATE bannerdream SET title=?, img=?, des=? WHERE id=?";
+        $stmt= $this -> $conn -> prepare($sql);
+        $stmt=bind_param("sssi",$title,$img,$des,$id);
+        if($stmt->execute())
+        {
+          return true;
+        }
+        else
+        {
+          throw new Exception("ERROR");
+        }
+        
+      }
+
+
+
+      
+?>
