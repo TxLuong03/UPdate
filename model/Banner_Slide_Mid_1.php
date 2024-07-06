32381e65d57b0d8090856a8f05c23ea7597973f3
Lương <133470672+TxLuong03@users.noreply.github.com>
2024-07-06 14:09:52 +0700

    Update Banner_Slide_Mid_1.php

--- model/Banner_Slide_Mid_1.php
+++ model/Banner_Slide_Mid_1.php
@@ -1,5 +1,20 @@
 <?php
   namespace AD\model;
-  use
+  use mysqli;
+  use Exception;
+  class BannerSlideMid1
+    {
+      private $conn;
+      public function con()
+      {
+        $servername="localhost";
+        $username="root";
+        $pass="";
+        $dbname="mysql";
+
+        $this -> $conn = new mysqli($servername,$username,$pass,$dbname);
+        
+      }
+    }
 
 ?>
