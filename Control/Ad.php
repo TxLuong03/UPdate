627a37b30a44dc7d0f378cc77fc120bfdb906796
Lương <133470672+TxLuong03@users.noreply.github.com>
2024-07-06 16:17:21 +0700

    Update Ad.php

--- Control/Ad.php
+++ Control/Ad.php
@@ -1,1 +1,56 @@
+<?php
+  namespace AD\Control;
+  use AD\model\Account;
+  class AdControl
+    {
+      public function logout()
+      {
+        session_unset();
+        session_destroy();
+        header("location: ?act=SignIn");
+        
+      }
+      public function SignIn()
+      {
+        $not="";
+        require_once "./view/SingIn.php";
+      }
+      public function SingUp()
+      {
+        $not="";
+        require_once "./view/SingUp.php"; 
+      }
+      public function processSingIn()
+      {
+        $acc=new Account();
+        if ( $_SERVER["REQUEST_METHOD" ] == " POST " )
+        {
+          $user = $_POST["user"];
+          $pass = $_POST["pass"];
+          if($acc -> SignIn($user,$pass))
+          {
+            $_SESSION["user"]=$user;
+            header ("location: ?act =homepage");
+          }
+          else
+          {
+              $not= " ERROR";
+          }
+            
+        }
+          
+        
+      }
+    }
+?>
+
+
+
+
+
+
+
+
+
+
 
