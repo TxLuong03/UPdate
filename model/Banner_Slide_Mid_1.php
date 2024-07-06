a0f538605d6e09058c54f948265ac5833bd29384
Lương <133470672+TxLuong03@users.noreply.github.com>
2024-07-06 15:52:32 +0700

    Update Banner_Slide_Mid_1.php

--- model/Banner_Slide_Mid_1.php
+++ model/Banner_Slide_Mid_1.php
@@ -15,6 +15,39 @@
         $this -> $conn = new mysqli($servername,$username,$pass,$dbname);
         
       }
+
+
+      public function getA()
+      {
+        $list=[];
+        $sql="SELECT * FROM bannerslidemid1";
+        $rs = $this -> $conn -> query($sql);
+        while ( $row -> num_rows >0 )
+          {
+            $list[]=$row;
+            
+          }
+        return $list;
+        
+      }
+
+      public function UpdateBannerSlideMid1()
+      {
+        $sql="UPDATE bannerslidemid1 SET title = ? , img=? des=?, WHERE id=? ";
+        $stmt = $this -> $conn -> prepare($sql);
+        $stmt->bind_param("sssi",$title,$img,$des,$id);
+        if($stmt->execute())
+        {
+          return true;
+          
+        }
+        else
+        {
+          throw new Exception("ERROR");
+          
+        }
+        
+      }
     }
 
 ?>
