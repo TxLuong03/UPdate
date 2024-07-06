15bf55ab7cb038bd110cb8750e76cb8d0adca734
Lương <133470672+TxLuong03@users.noreply.github.com>
2024-07-06 15:56:52 +0700

    Update Banner_Slide_Mid_2.php

--- model/Banner_Slide_Mid_2.php
+++ model/Banner_Slide_Mid_2.php
@@ -1,1 +1,53 @@
+<?php
+  namespace AD\model;
+  use mysqli;
+  use Exception;
+  class BannerSlideMid1
+    {
+      private $conn;
+      public function con()
+      {
+        $servername="localhost";
+        $username="root";
+        $pass="";
+        $dbname="mysql";
 
+        $this -> $conn = new mysqli($servername,$username,$pass,$dbname);
+        
+      }
+
+
+      public function getA()
+      {
+        $list=[];
+        $sql="SELECT * FROM bannerslidemid1";
+        $rs = $this -> $conn -> query($sql);
+        while ( $row -> num_rows >0 )
+          {
+            $list[]=$row;
+            
+          }
+        return $list;
+        
+      }
+
+      public function UpdateBannerSlideMid2($id,$title,$img,$des)
+      {
+        $sql="UPDATE bannerslidemid1 SET title = ? , img=? des=?, WHERE id=? ";
+        $stmt = $this -> $conn -> prepare($sql);
+        $stmt->bind_param("sssi",$title,$img,$des,$id);
+        if($stmt->execute())
+        {
+          return true;
+          
+        }
+        else
+        {
+          throw new Exception("ERROR");
+          
+        }
+        
+      }
+    }
+
+?>
