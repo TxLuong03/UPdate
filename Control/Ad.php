f185aa252f0066ef400ccdfea888d271990e9734
Lương <133470672+TxLuong03@users.noreply.github.com>
2024-07-08 09:24:37 +0700

    Update Ad.php

--- Control/Ad.php
+++ Control/Ad.php
@@ -41,6 +41,37 @@
           
         
       }
+      public function processSingUp()
+      {
+        $acc=new Account();
+        if ( $_SERVER["REQUEST_METHOD" ] == " POST " )
+        {
+          $user = $_POST["user"];
+          $pass = $_POST["pass"];
+          $re_pass=$_POST["re_pass"];
+          if($this->checkPassword($re_pass, $pass) == false)
+          {
+            $notice = "password and repasword is incorrect";
+            require_once "./view/SignUp.php";
+            exit();
+          }
+          if($acc->SingUp($user, $pass)){
+                header("Location:?act=Signin.php");
+            }else{
+                echo " failed";
+            }
+            
+        }
+          
+        
+      }
+      public function checkPassword($pass, $re_pass){
+        if($pass == $re_pass){
+            return true;
+        }else{
+            return false;
+        }
+    }
     }
 ?>
 
