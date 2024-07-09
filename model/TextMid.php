f41e8edef1a1f1f61e067fcc89aa4ede6fe1febd
Lương <133470672+TxLuong03@users.noreply.github.com>
2024-07-09 08:52:13 +0700

    Update TextMid.php

--- model/TextMid.php
+++ model/TextMid.php
@@ -14,27 +14,33 @@
 
         $this -> conn=new mysqli($nameserver,$username,$password,$dbname);
     }
-   public function getA(){
-        $listCart = [];
-        $sql = "SELECT * FROM textmid";
-        $result = $this->conn->query($sql);
-        while($row = $result->fetch_assoc()){
-            $listCart[] = $row;
+  public function getA() {
+    $list = [];
+    $sql = "SELECT * FROM textmid";
+    $result = $this->conn->query($sql);
+    if ($result) {
+        while ($row = $result->fetch_assoc()) {
+            $list[] = $row;
         }
-            return $listCart;
-        
+        $result->close(); 
+    } else {
+        throw new Exception("Query execution failed: " . $this->conn->error);
+    }
+    $this->conn->close();
+    return $list;
     }
 
-    public function updateTextMid($id ,$title, $des){
-        $sql = "UPDATE textmid SET title =?,des =? WHERE id =?";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bind_param("sssi", $title,$des, $id);
-        if($stmt->execute()){
-            return true;
-        } else{
-            throw new Exception("Error: ". $this->conn->error);
-        }
+
+   public function updateTextMid($id, $title, $des) {
+    $sql = "UPDATE textmid SET title = ?, des = ? WHERE id = ?";
+    $stmt = $this->conn->prepare($sql);
+    if ($stmt->execute([$title, $des, $id])) {
+        return true;
+    } else {
+        throw new Exception("Error: " . $this->conn->error);
     }
+    }
+
 
     }
 ?>
