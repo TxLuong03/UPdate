b72008d15a3bf7a1008997ac6eb96c0162bae3ce
Lương <133470672+TxLuong03@users.noreply.github.com>
2024-07-09 08:27:25 +0700

    Update Banner_Bottom.php

--- model/Banner_Bottom.php
+++ model/Banner_Bottom.php
@@ -40,23 +40,20 @@ class bannerbottom
         }
     }
 
-    public function updateBannerBottom($img){
-        try {
-            $stmt = $this->conn->prepare("UPDATE bannerbottom SET img =? WHERE id = 3");
-            $stmt->bind_param("s", $img);
-            if (!$stmt) {
-                throw new Exception("Prepare statement failed: " . $this->conn->error);
-            }
-            
-            $stmt->execute();
-            return true;
-        } catch (Exception $e) {
-            echo "Error: ". $e->getMessage();
-        } finally {
-            $stmt->close();
-            $this->conn->close();
+    public function updateBannerBottom($id, $img) {
+    try {
+        $stmt = $this->conn->prepare("UPDATE bannerbottom SET img = ? WHERE id = ?");
+        if (!$stmt) {
+            throw new Exception("Prepare statement failed: " . $this->conn->error);
         }
+        $stmt->execute([$img, $id]);
+        return true;
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    } finally {
+        $stmt->close();
+        $this->conn->close();
     }
-
+}
 }
 ?>
