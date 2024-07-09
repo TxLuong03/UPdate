16a0d5f316b8268bd58a8ad6ef1aaaea1ca1f252
Lương <133470672+TxLuong03@users.noreply.github.com>
2024-07-09 09:45:30 +0700

    Update Banner_Bottom.php

--- View_Ad/Banner_Bottom.php
+++ View_Ad/Banner_Bottom.php
@@ -44,10 +44,10 @@
                     <div class="form-group">
                         <label for="currentImg">Hình ảnh hiện tại</label>
                         <div class="mb-3">
-                            <img src="current_image.jpg" alt="Current Image" class="img-thumbnail" id="currentImg">
+                            <img src="<?php echo $bannerbottom->getBannerBottom() ?>" alt="Current Image" class="img-thumbnail" id="currentImg">
                         </div>
                         <label for="img">Chọn hình ảnh mới</label>
-                        <input type="file" class="form-control-file" id="img" name="img" value="<?php echo $bannerbottom->getBannerBottom() ?>">
+                        <input type="file" class="form-control-file" id="img" name="img" >
                     </div>
                     <button type="submit" class="btn btn-primary btn-block">Xác nhận sửa</button>
                 </form>
